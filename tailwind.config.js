cc6de477c871c29006b18df20cb475384db75b82
tin-kri <113044805+tin-kri@users.noreply.github.com>
2024-10-30 14:37:54 +0100

    staring feed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./**/*.{html,js,ts}", "!./node_modules/**/*"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {},
+      },
+    },
   },
   plugins: [],
-}
-
+};
