5e9bce96951f08c86fbbcd66164d36e9e274fc2c
tin-kri <113044805+tin-kri@users.noreply.github.com>
2024-11-11 11:43:22 +0100

    navbar og feed

---
+++ js/main.js
@@ -0,0 +1,10 @@
+import "../style.css";
+import { mobileNavigation } from "../js/mobileNav.js";
+
+mobileNavigation();
+
+document.querySelector("#landing-page").innerHTML = `
+  <div >
+     <h1 class=" text-red-950 text-center p-10 text-5xl font-bold tracking-wider">Mizioù</h1>
+  </div>
+`;
--- main.js
+++
@@ -1,9 +0,0 @@
-import "./style.css";
-import javascriptLogo from "./javascript.svg";
-import viteLogo from "/vite.svg";
-
-document.querySelector("#landing-page").innerHTML = `
-  <div>
-     <h1 class="p-10 text-5xl font-bold text-center tracking-wider">Mizioù</h1>
-  </div>
-`;
