14f6d21ea4d59a287b8c6a6f429a8659e786add6
tin-kri <113044805+tin-kri@users.noreply.github.com>
2025-01-30 20:10:27 +0100

    login api ok

--- src/js/api/auth/register.js
+++ src/js/api/auth/register.js
@@ -1,29 +1,32 @@
 import { API_AUTH_REGISTER } from '../../constants/constants.js'
 
-export function registerFrom() {
-    const registerFrom = document.querySelector('#register-form')
-    console.log('this is working register')
-}
+// export function registerForm() {
+//     const registerForm = document.querySelector('#register-form')
 
-export async function registerUser(userData) {
-    try {
-        const fetchOptions = {
-            method: 'POST',
-            body: 'JSON.stringify(userData)',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        }
-        const response = await fetch(API_AUTH_REGISTER, fetchOptions)
-        console.log(response)
-    } catch (error) {
-        console.log('Something went wrong with fetch'), error
-    }
-}
+//     registerForm.addEventListener('submit', function () {
+//         console.log('well hello there')
+//     })
+// // }
 
-export function onRegisterFormSubmit(event) {
-    event.preventDefault()
-    const formData = new FormData(event.target)
-    const formFields = Object.fromEntries(formData)
-    registerUser(formFields)
-}
+// export async function registerUser(userData) {
+//     try {
+//         const fetchOptions = {
+//             method: 'POST',
+//             body: 'JSON.stringify(userData)',
+//             headers: {
+//                 'Content-Type': 'application/json',
+//             },
+//         }
+//         const response = await fetch(API_AUTH_REGISTER, fetchOptions)
+//         console.log(response)
+//     } catch (error) {
+//         console.log('Something went wrong with fetch'), error
+//     }
+// }
+
+// export function onRegisterFormSubmit(event) {
+//     event.preventDefault()
+//     const formData = new FormData(event.target)
+//     const formFields = Object.fromEntries(formData)
+//     registerUser(formFields)
+// }
--- src/main.js
+++ src/main.js
@@ -4,17 +4,46 @@
 // import javascriptLogo from "./assets/img/javascript.svg";
 import './style.css'
 import { mobileNavigation } from './mobileNav'
-import { registerFrom } from './js/api/auth/register'
-import { registerUser } from './js/api/auth/register'
-import { onRegisterFormSubmit } from './js/api/auth/register'
+import { API_AUTH_REGISTER } from '../src/js/constants/constants'
+
+// import { registerForm } from './js/api/auth/register'
+// import { registerUser } from './js/api/auth/register'
+// import { onRegisterFormSubmit } from './js/api/auth/register'
 
 mobileNavigation()
-registerFrom()
-registerUser()
-registerFrom.addEventListener('submit', onRegisterFormSubmit)
+// registerForm()
+// registerUser()
 
 document.querySelector('#landing-page').innerHTML = `
   <div >
      <h1 class=" text-red-950 text-center p-10 text-5xl font-bold tracking-wider">Mizioù</h1>
   </div>
 `
+const registerForm = document.querySelector('#register-form')
+
+async function registerUser(userDetails) {
+    try {
+        const fetchOptions = {
+            method: 'POST',
+            body: JSON.stringify(userDetails),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        }
+
+        const response = await fetch(API_AUTH_REGISTER, fetchOptions)
+    } catch (error) {
+        console.log('there is an error', error)
+    }
+}
+
+// handler
+function onRegisterFormSubmit(event) {
+    event.preventDefault()
+    const formData = new FormData(event.target)
+    const formFields = Object.fromEntries(formData)
+    registerUser(formFields)
+    console.log(formFields)
+}
+
+registerForm.addEventListener('submit', onRegisterFormSubmit)
